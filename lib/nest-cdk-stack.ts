8b71a58f2d0a8ccb9b7edacaa40d1457dc2d5071
jimao <shihao.gu@perfma.com>
2024-11-16 22:08:21 +0800

    提交

--- lib/nest-cdk-stack.ts
+++ lib/nest-cdk-stack.ts
@@ -1,8 +1,42 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as amplify from 'aws-cdk-lib/aws-amplify';
+import { BuildSpec } from 'aws-cdk-lib/aws-codebuild';
 import { Construct } from 'constructs';
 
 export class NestCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const buildSpec = BuildSpec.fromObject({
+      version: 1,
+      applications: [{
+        frontend: {
+          phases: {
+            build: {
+              commands: ['npm ci', 'npm run build']
+            }
+          },
+          artifacts: {
+            baseDirectory: '.next',
+            files: ['**/*']
+          },
+          cache: {
+            paths: ['node_modules/**/*']
+          }
+        }
+      }]
+    });
+
+    const app = new amplify.CfnApp(this, 'AmplifyApp', {
+      name: 'my-app',
+      repository: 'https://github.com/your-repo',
+      accessToken: 'your-token',
+      buildSpec: buildSpec.toString()
+    });
+
+    new amplify.CfnBranch(this, 'MainBranch', {
+      appId: app.attrAppId,
+      branchName: 'main'
+    });
   }
 }
